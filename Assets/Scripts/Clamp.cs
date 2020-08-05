d7074d53ab2d470f93ee35003f881fd7456cd7d2
Pikaboi <56373816+Pikaboi@users.noreply.github.com>
2020-08-05 16:09:02 +1200

    clamp improved?

--- Assets/Scripts/Clamp.cs
+++ Assets/Scripts/Clamp.cs
@@ -4,20 +4,23 @@ using UnityEngine;
 
 public class Clamp : MonoBehaviour
 {
-    private Vector2 bounds;
 
     // Start is called before the first frame update
     void Start()
     {
-        bounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Camera.main.transform.position.z, Screen.height));
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        Vector3 viewPos = transform.position;
-        viewPos.x = Mathf.Clamp(viewPos.x, bounds.x, bounds.x * -1);
-        viewPos.z = Mathf.Clamp(viewPos.z, bounds.y, bounds.y * -1);
-        transform.position = viewPos;
+        if(transform.position.x > 50.0f)
+        {
+            transform.position = new Vector3(50.0f, transform.position.y, transform.position.z);
+        }
+
+        if(transform.position.x < -50.0f)
+        {
+            transform.position = new Vector3(-50.0f, transform.position.y, transform.position.z);
+        }
     }
 }
