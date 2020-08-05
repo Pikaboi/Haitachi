7ae61db2e95af88029bd28987822ae5aa11289d0
Pikaboi <56373816+Pikaboi@users.noreply.github.com>
2020-08-05 16:50:57 +1200

    clamp is hard

--- Assets/Scripts/Clamp.cs
+++ Assets/Scripts/Clamp.cs
@@ -4,23 +4,44 @@ using UnityEngine;
 
 public class Clamp : MonoBehaviour
 {
-
+    Camera cam;
+    Vector3 screenPos;
     // Start is called before the first frame update
     void Start()
     {
+        cam = Camera.main;
+        screenPos = cam.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height));
+        Debug.Log("target is " + screenPos.x + " pixels from the left" + screenPos.y);
+
+        transform.position = screenPos;
     }
 
     // Update is called once per frame
-    void LateUpdate()
+    void Update()
     {
-        if(transform.position.x > 50.0f)
+        //Vector3 view = transform.position;
+
+        //view.x = Mathf.Clamp(view.x, screenPos.x, screenPos.x);
+
+
+        /*if(transform.position.x > (cam.fieldOfView / 4))
         {
-            transform.position = new Vector3(50.0f, transform.position.y, transform.position.z);
+            transform.position = new Vector3((cam.fieldOfView / 4), transform.position.y, transform.position.z);
         }
 
-        if(transform.position.x < -50.0f)
+        if(transform.position.x < -(cam.fieldOfView / 4))
         {
-            transform.position = new Vector3(-50.0f, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-(cam.fieldOfView / 4), transform.position.y, transform.position.z);
         }
+
+        if (transform.position.z > (cam.fieldOfView / 4))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, (cam.fieldOfView / 4));
+        }
+
+        if (transform.position.z < -(cam.fieldOfView / 4))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -(cam.fieldOfView / 4));
+        }*/
     }
 }
