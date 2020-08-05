07bba82104a2ea2a7a8024de5556d9c7ddeaf736
RachaelColaco <65530256+RachaelColaco@users.noreply.github.com>
2020-08-05 15:59:03 +1200

    We infected

--- Assets/Scripts/LeaderFollowingAI.cs
+++ Assets/Scripts/LeaderFollowingAI.cs
@@ -6,7 +6,7 @@ public class LeaderFollowingAI : MonoBehaviour
     public float speed;
     private Transform target;
     public Rigidbody rb;
-
+    private bool isInfected = false;
 
     // Start is called before the first frame update
     void Start()
@@ -21,35 +21,47 @@ public class LeaderFollowingAI : MonoBehaviour
     void Update()
     {
         rb.velocity = new Vector3(0,0,0);
-        //float distance = Vector2.Distance(transform.forward, target.transform.forward);
-        float dist = Vector3.Distance(transform.position, target.position);
-        Debug.Log("Distance to other: " + dist);
-
-        if (dist < 4.0f)
-        { 
-            Vector3 dirToPlayer = transform.position - target.transform.position;
-            Vector3 newPos = transform.position + dirToPlayer;
-            Quaternion targetRotation = Quaternion.LookRotation(target.transform.position - transform.position);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.deltaTime);
-            transform.position += transform.right * speed * Time.deltaTime;
-            // transform.position += newPos;
-        }
-        else if(dist<6.0f)
+        if (isInfected)
         {
-            //Vector3 dirToPlayer = transform.position - target.transform.position;
-            //Vector3 newPos = transform.position + dirToPlayer;
-            //transform.position -= newPos;
-        }
-        else
-        {
-            Quaternion targetRotation = Quaternion.LookRotation(target.transform.position - transform.position);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.deltaTime);
-            transform.position += transform.forward * speed * Time.deltaTime;
-        }
-        //transform.LookAt(target.transform);
-        //transform.position += transform.forward * 1f * Time.deltaTime;
+            //float distance = Vector2.Distance(transform.forward, target.transform.forward);
+            float dist = Vector3.Distance(transform.position, target.position);
+            Debug.Log("Distance to other: " + dist);
 
-        
+            if (dist < 4.0f)
+            {
+                Vector3 dirToPlayer = transform.position - target.transform.position;
+                Vector3 newPos = transform.position + dirToPlayer;
+                Quaternion targetRotation = Quaternion.LookRotation(target.transform.position - transform.position);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.deltaTime);
+                transform.position += transform.right * speed * Time.deltaTime;
+                // transform.position += newPos;
+            }
+            else if (dist < 6.0f)
+            {
+                //Vector3 dirToPlayer = transform.position - target.transform.position;
+                //Vector3 newPos = transform.position + dirToPlayer;
+                //transform.position -= newPos;
+            }
+            else
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(target.transform.position - transform.position);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.deltaTime);
+                transform.position += transform.forward * speed * Time.deltaTime;
+            }
+            //transform.LookAt(target.transform);
+            //transform.position += transform.forward * 1f * Time.deltaTime;
+
+        }
 
     }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "Player" && isInfected == false)
+        {
+            Debug.Log("we hit something");
+            isInfected = true;
+        }
+        
+    }
+
 }
