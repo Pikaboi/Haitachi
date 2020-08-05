7625b897fcbef45e8df5973288e6ee974088d377
RachaelColaco <65530256+RachaelColaco@users.noreply.github.com>
2020-08-05 17:26:21 +1200

    Update FleeAI.cs

--- Assets/Scripts/FleeAI.cs
+++ Assets/Scripts/FleeAI.cs
@@ -27,7 +27,7 @@ public class FleeAI : MonoBehaviour
 
             float dist = Vector3.Distance(transform.position, target.position);
             Debug.Log("Distance to other: " + dist);
-            if (dist < 4.0f)
+            if (dist < 6.0f)
             {
                 Quaternion targetRotation = Quaternion.LookRotation(transform.position - target.transform.position);
                 transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 5.0f * Time.deltaTime);
