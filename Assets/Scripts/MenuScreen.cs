23489782cb4859283b351d9d09d08c2090cf22fb
RachaelColaco <65530256+RachaelColaco@users.noreply.github.com>
2020-08-12 10:25:44 +1200

    Menudone

--- Assets/Scripts/MenuScreen.cs
+++ Assets/Scripts/MenuScreen.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class MenuScreen : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -14,5 +14,10 @@ public class MenuScreen : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
     }
 }
