48cdc8b0d07211bfa415e15a304ed5563340492f
Pikaboi <56373816+Pikaboi@users.noreply.github.com>
2020-08-12 17:30:52 +1200

    animation!

--- Assets/Scripts/MenuScreen.cs
+++ Assets/Scripts/MenuScreen.cs
@@ -5,20 +5,38 @@ using UnityEngine.SceneManagement;
 
 public class MenuScreen : MonoBehaviour
 {
+    public Animator anim;
+    private bool StartBool = false;
+    private float timer = 1.3f;
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        if (StartBool == true)
+        {
+            timer = timer - Time.deltaTime;
+            if (timer <= 0)
+            {
+                SceneManager.LoadScene(1);
+            }
+            Debug.Log(timer);
+        }
         
     }
 
     public void StartGame()
     {
-        SceneManager.LoadScene(1);
+        anim.SetTrigger("Clap");
+        StartBool = true;
     }
 }
