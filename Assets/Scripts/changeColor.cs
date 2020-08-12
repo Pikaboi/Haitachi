8563cdd8a64a2926c15e0fca86dc42b1d1c5e576
RachaelColaco <65530256+RachaelColaco@users.noreply.github.com>
2020-08-12 19:03:37 +1200

    workingoncolorchange

---
+++ Assets/Scripts/changeColor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeColor : MonoBehaviour
+{
+    public Material[] materials;
+    public Renderer rend;
+    public LeaderFollowingAI AIscriptName;
+    // Material[] materials1101 = GetComponent<Renderer>().material;
+    //public Material m_Material_line;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        //GetComponent<Renderer>().material.color = Color.red;
+        //m_Material_body = GetComponent<Renderer>().material;
+        //m_Material_body.color =  Color.red;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (AIscriptName.getInfected())
+        {
+            GetComponent<Renderer>().material = materials[1];
+        }
+        //rend.sharedMaterial = materials[0];
+    }
+}
