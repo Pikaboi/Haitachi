0558c5a6598e1664892eef92474daf19c80f9792
Pikaboi <56373816+Pikaboi@users.noreply.github.com>
2020-10-17 11:48:58 +1300

    lobby set up

---
+++ Assets/Transition.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Transition : MonoBehaviour
+{
+    public int transition = 0;
+    void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(transition);
+    }
+}
