a95987649c3466631653a77dc9498d784ff9e013
Pikaboi <56373816+Pikaboi@users.noreply.github.com>
2020-10-28 10:56:44 +1300

    added newer objs and infection

---
+++ Assets/Scripts/Infector.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Infector : MonoBehaviour
+{
+    public ParticleSystem sys;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sys.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void beginInfect()
+    {
+        sys.Play();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.GetComponent<Infector>() != null)
+        {
+            other.gameObject.GetComponent<Infector>().beginInfect();
+        }
+    }
+}
