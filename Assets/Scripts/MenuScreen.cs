cd8170c3a1f9dc3125ce0c5e1e47a4836fc19973
Pikaboi <56373816+Pikaboi@users.noreply.github.com>
2020-11-04 22:06:47 +1300

    menu is quittable

--- Assets/Scripts/MenuScreen.cs
+++ Assets/Scripts/MenuScreen.cs
@@ -9,9 +9,11 @@ public class MenuScreen : MonoBehaviour
     GlobalController cont;
 
     public GameObject text;
+    public GameObject quitter;
 
     private Vector2 mousePos = new Vector2(500.0f, 350.0f);
     private bool start = false;
+    private bool quit = false;
 
     void Awake()
     {
@@ -35,17 +37,31 @@ public class MenuScreen : MonoBehaviour
         cont.controller.Menu.Start.performed += ctx => start = true;
         cont.controller.Menu.Start.canceled += ctx => start = false;
 
+        cont.controller.Menu.Quit.performed += ctx => quit = true;
+        cont.controller.Menu.Quit.canceled += ctx => quit = false;
+
         transform.position = new Vector3(mousePos.x, mousePos.y, 0);
 
-        if(Vector3.Distance(transform.position, text.transform.position) <= 20.0f)
+        if(Vector3.Distance(transform.position, text.transform.position) <= 10.0f)
         {
             cont.keys.Menu.Hold.performed += ctx => start = true;
             cont.keys.Menu.Hold.canceled += ctx => start = false;
         }
 
-        if(start == true)
+        if (Vector3.Distance(transform.position, quitter.transform.position) <= 10.0f)
+        {
+            cont.keys.Menu.Hold.performed += ctx => quit = true;
+            cont.keys.Menu.Hold.canceled += ctx => quit = false;
+        }
+
+        if (start == true)
         {
             SceneManager.LoadScene(1);
+        }
+
+        if(quit == true)
+        {
+            Application.Quit();
         }
 
     }
